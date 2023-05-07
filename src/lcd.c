512ca16025a86cec6ce25401baf663dd3e94c901
Rob Prouse <rob@prouse.org>
2023-05-06 22:41:39 -0400

    Write to the LCD in C this time

--- src/lcd.c
+++ src/lcd.c
@@ -1,115 +1,68 @@
-
-// zcc +cpm -vn -O3 -clib=new lcd.c -o lcd -create-app
+// zcc +cpm +cpm -O3 -olcd.com lcd.c
 
 #include <stdio.h>
-#include <string.h>
+#include <stdlib.h>
 
-#asm
+#define lcd_comm_port 0x20  // Port addresses. Change as needed.
+#define lcd_data_port 0x21
 
-lcd_comm_port equ $20  ;Port addresses. Change as needed.
-lcd_data_port equ $21
+#define lcd_set_8bit 0x3F   // 8-bit port, 2-line display
+#define lcd_cursor_on 0x0F  // Turn cursors on
+#define lcd_cls 0x01        // Clear the display
 
-lcd_set_8bit equ $3f   ;8-bit port, 2-line display
-lcd_cursor_on equ $0f  ;Turn cursors on
-lcd_cls equ $01        ;Clear the display
+#define lcd_line1 128       // CMD to move the cursor to the first char of line 1
+#define lcd_line2 192
+#define lcd_line3 148
+#define lcd_line4 212
 
-lcd_line1 equ 128      ; CMD to move the cursor to the first char of line 1
-lcd_line2 equ 192
-lcd_line3 equ 148
-lcd_line4 equ 212
+#define busy_flag 0x80
 
-lcd_init:
-  ;Initialize the LCD and set cursor to line 1
-  ld a,lcd_set_8bit
-  call lcd_send_command
-
-  ld a,lcd_cursor_on
-  call lcd_send_command
-
-  ld a,lcd_cls
-  call lcd_send_command
-
-  ; Go to line 1
-  ld a,lcd_line1
-  call lcd_send_command
-
-;******************
-;Send a command byte to the LCD
-;Entry: A= command byte
-;Exit: All preserved
-;******************
-lcd_send_command:
-  push bc              ;Preserve
-  ld c,lcd_comm_port   ;Command port
-
-.lcd_command_wait_loop:   ;Busy wait
-  in b,(c)             ;Read status byte
-  sll b                ;Shift busy bit into carry flag
-  jr c,.lcd_command_wait_loop    ;While busy
-
-  out (c),a            ;Send command
-  pop bc               ;Restore
-  ret
-
-;******************
-;Send a data byte to the LCD
-;Entry: A= data byte
-;Exit: All preserved
-;******************
-lcd_send_data:
-  push bc              ;Preserve
-  ld c,lcd_comm_port   ;Command port
-
-.lcd_data_wait_loop:      ;Busy wait
-  in b,(c)             ;Read status byte
-  sll b                ;Shift busy bit into carry flag
-  jr c,.lcd_data_wait_loop    ;While busy
-
-  ld c,lcd_data_port   ;Data port
-  out (c),a            ;Send data
-  pop bc               ;Restore
-  ret
-
-;******************
-;Send an asciiz string to the LCD
-;Entry: HL=address of string
-;Exit: HL=address of ending zero of the string. All others preserved
-;******************
-lcd_send_asciiz:
-  push af
-  push bc              ;Preserve
-.lcd_asciiz_char_loop:
-  ld c,lcd_comm_port   ;Command port
-
-.lcd_asciiz_wait_loop:    ;Busy wait
-  in a,(c)             ;Read status byte
-  rlca                 ;Shift busy bit into carry flag
-  jr c,.lcd_asciiz_wait_loop    ;While busy
-
-  ld a,(hl)            ;Get character
-  and a                ;Is it zero?
-  jr z,.lcd_asciiz_done ;If so, were done
-
-  ld c,lcd_data_port   ;Data port
-  out (c),a            ;Send data
-  inc hl               ;Next char
-  jr .lcd_asciiz_char_loop
-
-.lcd_asciiz_done:
-  pop bc               ;Restore
-  pop af
-  ret
-
-#endasm
-
-void LcdInit()
+void lcd_busy_wait()
 {
-    #asm
-    call lcd_init
-    #endasm
+    while(inp(lcd_comm_port) & busy_flag);
+}
+
+// Send a command byte to the LCD
+void lcd_send_command(uint8_t command)
+{
+    lcd_busy_wait();
+    outp(lcd_comm_port, command);
+}
+
+void lcd_init()
+{
+    // Initialize the LCD and set cursor to line 1
+    lcd_send_command(lcd_set_8bit);
+    lcd_send_command(lcd_cursor_on);
+    lcd_send_command(lcd_cls);
+    lcd_send_command(lcd_line1);
+}
+
+
+// Send a data byte to the LCD
+void lcd_send_data(uint8_t data)
+{
+    lcd_busy_wait();
+    outp(lcd_data_port, data);
+}
+
+// Send an asciiz string to the LCD
+void lcd_send_string(char* str)
+{
+    for (int i=0; str[i]; i++)
+    {
+        lcd_send_data(str[i]);
+    }
 }
 
 int main(void)
 {
-    LcdInit();
+    lcd_init();
+    lcd_send_string("RC2014 Z80 Computer");
+    lcd_send_command(lcd_line2);
+    lcd_send_string("29752 Bytes free");
+    lcd_send_command(lcd_line3);
+    lcd_send_string("READY");
+    lcd_send_command(lcd_line4);
+    lcd_send_string(">");
 }
