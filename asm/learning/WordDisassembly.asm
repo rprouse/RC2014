ff460aab493251c5a90e0585f0230a9989440335
Rob Prouse <rob@prouse.org>
2023-05-22 16:18:58 -0400

    Fix mistake in Word Disassembly

--- asm/learning/WordDisassembly.asm
+++ asm/learning/WordDisassembly.asm
@@ -1,9 +1,7 @@
-
 ; Store high nibble of (op1) at (res1) and low nibble at (res2)
   org 100H      ; CPM Program start address
 
-  ld hl,op1     ; Set a pointer to op1
-  ld a,(hl)     ; Get the data for op1
+  ld a,(op1)    ; Get the data for op1
   ld b,a        ; Make a copy for later
 
   rra           ; Shift right 4 times
@@ -11,13 +9,11 @@
   rra
   rra
   and 0FH       ; Mask off the high bits
-  inc hl        ; Point HL at res1
-  ld a,(hl)     ; Save the result
+  ld (res1),a   ; Save the result
 
   ld a,b        ; Grab the copy
   and 0FH       ; Mask off the high bits
-  inc hl        ; Point HL at res2
-  ld a,(hl)     ; Save the result
+  ld (res2),a   ; Save the result
   ret
 
 op1:  db 3EH
