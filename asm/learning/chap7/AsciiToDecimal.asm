635285ac816851d2db391cb9b38f75e474308188
Rob Prouse <r.prouse@valsoftcorp.com>
2023-05-29 20:57:21 -0400

    Convert an ASCII value to a decimal value

---
+++ asm/learning/chap7/AsciiToDecimal.asm
@@ -0,0 +1,21 @@
+; Convert an ASCII number to a decimal value and store to
+; result. If the value is out of range, store FF
+  org 100H
+
+  ld a,(data)
+  sub '0'
+  jp m,invalid  ; out of range low?
+  cp 10
+  jr c,done     ; is out of range high?
+
+invalid:
+  ld a,0FFH
+
+done:
+  ld (res),a
+  ret
+
+; data: db 2BH  ; Out of range low
+; data: db 37H
+data: db 55H  ; Out of range high
+res:  db 0   ; Should be 7 or FF
