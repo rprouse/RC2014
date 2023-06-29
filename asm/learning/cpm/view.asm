25800e72055bc233280b82fe9412d315606b4535
Rob Prouse <rob@prouse.org>
2023-06-28 23:08:55 -0400

    Two programs to help understand command line processing in CP/M

--- asm/learning/cpm/view.asm
+++ asm/learning/cpm/view.asm
@@ -1,32 +1,52 @@
-; Views a file
+; Views a file passed in on the command line
 
-  org 100H      ; CPM Program start address
+; =============================================================================
+; Base page memory declarations in the first 255 bytes of memory
+boot        equ $0          ; The CP/M warm boot vector
+bdos        equ $5          ; CP/M BDOS vector address
+fcb         equ $5C         ; CP/M default File Control Block (FCB)
+dma         equ $80         ; default dma buffer, 128 bytes long
 
-  jp main
+; =============================================================================
+; BDOS functions
+c_write     equ $02         ; console output
+c_writestr  equ $09         ; output string, '$' terminated, to console output
+f_open      equ $0f         ; open file
+f_close     equ $10         ; close file
+f_read      equ $14         ; read next file record
 
-  include "cpm.inc"
+; =============================================================================
+; Other defines
+subz        equ $1A         ; SUB or Ctrl-Z
+space       equ $20         ; Space character
+
+; =============================================================================
+  org $100                  ; CPM Program start address
+  jp main                   ; skip over the data block
+
+data:
+; Error messages
+usage:      db "Usage: VIEW [DRIVE:]<FILENAME.EXT>\r\n$"
+err_open:   db "Failed to open file\r\n$"
+err_read:   db "Error reading file\r\n$"
 
 main:
-  ; Set up the FCB
-  ld hl, fcbdr
-  ld (hl), 0 ; Use default drive
-  ld hl, filename
-  ld de, (fcbfn)
-  ld bc, 7
-  ldir
-  ld hl, filetype
-  ld de, (fcbtn)
-  ld bc, 3
-  ldir
-  ld hl, fcbex
-  ld (hl), 0 ; Set to 0 when opening a file
-  ld hl, fcbrc
-  ld (hl), 0 ; Set to 0 when opening a file
+  ; Check the FCB contains a file
+  ld hl, fcb
+  inc hl
+  ld a, (hl)                ; Load the first char of the filename
+  cp space
+  jp nz, open_file          ; If it is a space, no file was given on the cmdline
+  ld de, usage
+  ld c, c_writestr
+  call bdos                 ; Display usage info
+  jp boot                   ; Warm boot CP/M
 
+open_file:
   ; Call BDOS function to open the file
   ld de, fcb
-  ld c, F_OPEN  ; Function number for "open file"
-  call BDOS
+  ld c, f_open              ; Function number for "open file"
+  call bdos
 
   ; Check if the file was opened successfully
   ld a, l
@@ -34,60 +54,40 @@ main:
   jp z, file_opened_successfully
 
   ; Handle error opening file
-  ld hl, err_open
-  call PRINT_STR
-  ret
+  ld de, err_open
+  ld c, c_writestr
+  call bdos
+  jp boot                   ; Warm boot CP/M
 
 file_opened_successfully:
   ; File is now open for reading
-  ; Set the DMA address to read into
-  ld de, buffer
-  ld c, F_SETDMA
-  call BDOS
-
   ; Start reading in the data 128 bytes at a time
   ld de, fcb
-  ld c, F_READ
-  call BDOS
-  cp a, 1        ; 1 is EOF
+  ld c, f_read
+  call bdos
+  cp a, 1                   ; 1 is EOF
   jr z, eof
-  cp a, 0        ; 0 is success
+  cp a, 0                   ; 0 is success
   jr nz, read_error
 
   ; Successfully read 128 bytes, output it
-  ld hl, buffer
+  ld hl, dma
   ld bc, 128
 write_loop:
-    ld a, (hl)
-    inc hl
-    ld e, a
-    ld c, C_WRITE
-    call BDOS
-    djnz write_loop
+  ld a, (hl)
+  inc hl
+  cp subz                   ; Is it a SUB/Ctrl+Z char?
+  jp z, eof                 ; Stop output
+
+  ld e, a
+  ld c, c_write
+  call bdos
+  djnz write_loop
 
 read_error:
-  ld hl, err_read
-  call PRINT_STR
-  ret
+  ld de, err_read
+  ld c, c_writestr
+  call bdos
 
 eof:
-  ret
-
-; Error messages
-err_open: db "Failed to open file\r\n$"
-err_read: db "Error reading file\r\n$"
-
-filename: db "READ     "
-filetype: db "ME "
-; fcb - File control block definitions
-fcb:    defs 36,0
-fcbdr   equ fcb+00h   ; Drive. 0 for default, 1-16 for A-P.
-fcbfn   equ fcb+01h   ; Filename, 7-bit ASCII.
-fcbtn   equ fcb+09h   ; Filetype, 7-bit ASCII.
-fcbex   equ fcb+0Ch   ; Set this to 0 when opening a file and then leave it
-fcbrc   equ fcb+0Fh   ; Set this to 0 when opening a file and then leave it
-fcbal   equ fcb+10h   ; Image of the second half of the directory entry
-fcbcr   equ fcb+20h   ; Current record within extent.
-fcbin   equ fcb+21h   ; Fcb length
-
-buffer: defs 128      ; Allocate 128 byte buffer for file data
+  jp boot                   ; Warm boot CP/M
