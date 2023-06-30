59fd1b4e099313d063eaf3bc057d088620da07d3
Rob Prouse <r.prouse@valsoftcorp.com>
2023-06-30 17:42:34 -0400

    Dump the memory

--- asm/cpm/mem.asm
+++ asm/cpm/mem.asm
@@ -35,10 +35,47 @@ loop:
   ld a, l                   ; Write a newline every 16 bytes
   and $0F
   cp 0
+  call z, dump_bytes
   call z, write_newline
 
   djnz loop
 
+  ret
+
+; =============================================================================
+; Dump the last 16 bytes
+dump_bytes:
+  push hl
+  push de
+  push bc
+  push af
+
+  ld a, space
+  call write_char
+
+  ld de, $FFF0              ; Subtract $0010 (2's compliment add)
+  add hl, de
+  ld b, $F
+
+dump_loop:
+  ld a, (hl)
+  inc hl
+
+  cp $20                    ; Compare A to the lower bound of printable ASCII
+  jr c, not_printable       ; If A is less than 0x20, it's not printable
+  cp $7E                    ; Compare A to the upper bound of printable ASCII
+  jr nc, not_printable      ; If A is greater than 0x7E, it's not printable
+  jr end_check
+not_printable:
+  ld a, '.'                 ; A is not printable ASCII
+end_check:
+  call write_char
+  djnz dump_loop
+
+  pop af
+  pop bc
+  pop de
+  pop hl
   ret
 
 ; =============================================================================
